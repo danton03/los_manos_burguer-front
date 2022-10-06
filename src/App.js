f09d7584c1d9bbef37374a38483693ae798284d4
Danton Matheus <37675973+danton03@users.noreply.github.com>
2022-10-06 12:16:23 -0300

    style: :lipstick: Change default font family to Roboto.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
 import {
-	ChakraProvider,
-	Box,
-	Text,
-	VStack,
-	Grid,
-	theme,
-} from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
+	BrowserRouter,
+	Routes,
+	Route
+} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import customTheme from "./assets/styles/customTheme";
 
 function App() {
 	return (
-		<ChakraProvider theme={theme}>
-			<Box textAlign="center" fontSize="xl">
-				<Grid minH="100vh" p={3}>
-					<ColorModeSwitcher justifySelf="flex-end" />
-					<VStack spacing={8}>
-						<Text>
-              Hello World!
-						</Text>
-					</VStack>
-				</Grid>
-			</Box>
+		<ChakraProvider theme={customTheme}>
+			<BrowserRouter>
+				<Routes>
+					<Route  path="/login" element={<LoginPage />}/>
+				</Routes>
+			</BrowserRouter>
 		</ChakraProvider>
 	);
 }
