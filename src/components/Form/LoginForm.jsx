7bf54b6fc8d3d36634cdb3329a5a715d4afafbdf
Danton Matheus <37675973+danton03@users.noreply.github.com>
2022-10-07 23:51:47 -0300

    feat: :sparkles: add a button to navigate to the signup page

--- src/components/Form/LoginForm.jsx
+++ src/components/Form/LoginForm.jsx
@@ -1,14 +1,18 @@
 import {
 	Button,
+	Flex,
 	Input,
-	VStack
+	VStack,
+	Link
 } from "@chakra-ui/react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function LoginForm(){
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [isDisabled, setIsDisabled] = useState(false);
+	const navigate = useNavigate();
 
 	function handleSubmit(e) {
 		e.preventDefault();
@@ -18,8 +22,8 @@ export default function LoginForm(){
 	}
 
 	return(
-		<form onSubmit={handleSubmit}>
-			<VStack spacing={4} maxW={600} >
+		<Flex flexDirection={"column"} gap={4}>
+			<VStack spacing={4} maxW={600} as={"form"} onSubmit={handleSubmit} >
 				<Input 
 					type="email" 
 					placeholder="E-mail"
@@ -49,6 +53,20 @@ export default function LoginForm(){
           Entrar
 				</Button>
 			</VStack>
-		</form>
+			<Link
+				as={"button"}
+				onClick={() => navigate("/signup")}
+				disabled={isDisabled}
+				background={"none"}
+				color={"orange.400"}
+				variant="none"
+				fontFamily={"'Silkscreen', cursive"}
+				fontSize={"sm"}
+				textDecoration={"underline"}
+				pt={4}
+			>
+				{"Não tem uma conta? cadastre-se"}
+			</Link>
+		</Flex>
 	);
 }
