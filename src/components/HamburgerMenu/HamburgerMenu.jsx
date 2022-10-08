d6b93d55ba0038bf92107d3f58c694174302b0de
Danton Matheus <37675973+danton03@users.noreply.github.com>
2022-10-07 23:36:02 -0300

    feat: :sparkles: create the hamburger menu component

---
+++ src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -0,0 +1,88 @@
+import { 
+	Button, 
+	Drawer, 
+	DrawerBody, 
+	DrawerCloseButton, 
+	DrawerContent, 
+	DrawerHeader, 
+	DrawerOverlay, 
+	useDisclosure,
+	VStack
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+
+export default function HamburgerMenu(){
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const navigate = useNavigate();
+	return(
+		<>
+			<Button
+				type="button"
+				onClick={onOpen}
+				w={"fit-content"}
+				padding={0}
+				background={"none"}
+				variant="ghost" 
+				color={"orange.400"}
+				colorScheme={"orange"}
+			>
+				<ion-icon name="menu" stlyle={{color: "orange"}} size="large"></ion-icon>
+			</Button>
+			<Drawer placement={"right"} onClose={onClose} isOpen={isOpen}>
+				<DrawerOverlay />
+				<DrawerContent>
+					<DrawerCloseButton />
+					<DrawerHeader 
+						borderBottomWidth="1px" 
+						fontFamily={"'Silkscreen', cursive"} 
+						color={"orange.400"}
+					>
+						Menu
+					</DrawerHeader>
+					<DrawerBody>
+						<VStack spacing={2}>
+							<Button 
+								type="button"
+								background={"none"}
+								color={"orange.400"}
+								colorScheme={"orange"}
+								variant="ghost"
+								fontFamily={"'Silkscreen', cursive"}
+								w={"100%"}
+								onClick={() => navigate("/login")}
+							>
+								Login
+							</Button>
+
+							<Button
+								type="button"
+								background={"none"}
+								color={"orange.400"}
+								colorScheme={"orange"}
+								variant="ghost"
+								fontFamily={"'Silkscreen', cursive"}
+								w={"100%"}
+								onClick={() => navigate("/signup")}
+							>
+								Cadastrar-se
+							</Button>
+
+							<Button
+								type="button"
+								background={"none"}
+								color={"orange.400"}
+								colorScheme={"orange"}
+								variant="ghost"
+								fontFamily={"'Silkscreen', cursive"}
+								w={"100%"}
+								onClick={() => navigate("/")}
+							>
+								Home
+							</Button>
+						</VStack>
+					</DrawerBody>
+				</DrawerContent>
+			</Drawer>
+		</>
+	);
+}
