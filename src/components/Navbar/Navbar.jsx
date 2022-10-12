573cf55c0771cad0343357026119f44a2a26a4e0
Danton Matheus <37675973+danton03@users.noreply.github.com>
2022-10-12 18:26:01 -0300

    feat: :sparkles: add a button to the cart page in the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,31 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 import { ColorModeSwitcher } from "../../ColorModeSwitcher";
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 
 export default function Navbar(){
+	const navigate = useNavigate();
 	return(
-		<Flex justifyContent={"space-between"} w={"100%"}>
+		<Flex justifyContent={"space-between"} w={"100%"} alignItems={"flex-start"}>
 			<ColorModeSwitcher />
-			<HamburgerMenu />
+			<Flex 
+				gap={3} 
+				alignItems={"center"} 
+				justifyContent={"center"}
+			>
+				<Button type="button"
+					onClick={() => navigate("/cart")}
+					padding={0}
+					background={"none"}
+					variant="ghost" 
+					color={"orange.400"}
+					colorScheme={"orange"}
+					w={6}
+				>
+					<ion-icon name="cart-outline" size={"large"} />
+				</Button>
+				<HamburgerMenu />
+			</Flex>
 		</Flex>
 	);
 }
